873317ab25282c7c957b77637a9217b3e94ef540
Hkhanke <hans.hanke@rub.de>
2024-12-08 16:07:09 +0100

    added KeyProviderImpl

---
+++ src/main/java/structra/assignment/task/impl/KeyProviderImpl.java
@@ -0,0 +1,13 @@
+package structra.assignment.task.impl;
+
+import structra.assignment.framework.llm.KeyProvider;
+
+public class KeyProviderImpl implements KeyProvider {
+    //Constructor
+    public KeyProviderImpl() {}
+
+    @Override
+    public String getApiKey(){
+
+    }
+}
